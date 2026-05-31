042e279c11e7b7d5e842bdf64b5f1f4f41123e47
chaitanya jadhab <chaitanya0jadhav1@gmail.com>
2026-05-31 12:45:53 +0530

    typescript issue resolved

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse", "pdfjs-dist", "@napi-rs/canvas"],
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/pdf-parse/lib/**/*"],
+  },
 };
 
 export default nextConfig;
